7bdd9192412d23aaca303be5a50b3300e896de65
MWILLHU <378b@M.local>
2014-11-06 14:08:34 -0500

    commit
    d

---
+++ ComDir.java
@@ -0,0 +1,84 @@
+
+public class ComDir {
+	
+	private static String name; //name of the community director
+	private static Dorm dorm; //name of the dorm director is assigned to
+	
+	ComDir(Dorm d, String n){
+		setDorm(d);
+		setName(n);		
+	}
+	
+	public void setDorm(Dorm d){
+		dorm = d;
+	}
+	
+	public void setName(String n){
+		name = n;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public String[] viewRoster(){ //returns array containing name of all residents in order by room
+		return dorm.getRoster();
+	}
+	
+	public void viewOccupancy(){ //returns number of rooms available
+		System.out.println(dorm.getAvailRooms() + " rooms are available in " + dorm.getName());
+	}
+	
+	public void viewResident(int roomNum){ //returns name of resident in room
+		System.out.println(dorm.getResident(roomNum) + " is the resident of room " + roomNum);
+	}
+	
+	public void checkInResident(String name, int roomNum){ //puts resident in room roomNum
+		/*if(student == checkedIn){					//pseudo that checks if resident is checked in elsewhere. checks them out of that room
+			if(student.dorm != dorm){				//and checks them into desired room
+				student.checkOut();
+				//throw error?
+			}
+			else{
+				dorm.setResident(name, roomNum);
+				dorm.setRoomVacant(roomNum);
+			}
+		}
+		else*/
+		{
+			if(dorm.getResident(roomNum) == null){
+				dorm.setResident(name, roomNum);
+				System.out.println(name + " is now residing in room " + roomNum);
+			}
+			else
+				System.out.println("Room " + roomNum + " is currently occupied. " + name + " cannot move into this room.");
+		}
+	}
+	
+	public void setRoomVacant(int roomNum){ //sets room vacant
+		if(dorm.getResident(roomNum) != null){
+			dorm.setRoomVacant(roomNum);
+			System.out.println("Room " + roomNum + " is empty and ready for check-in");
+		}
+		else{
+			System.out.println("Room " + roomNum + " is already empty");
+		}
+	}
+	
+	public void setRoomUnacceptable(int roomNum){ //sets room unacceptable, meaning residents cannot check into it
+		if(dorm.getResident(roomNum) != null){
+			System.out.println("Room " + roomNum + " is currently occupied. Will be set unacceptable anyway, student evicted");
+			dorm.setRoomVacant(roomNum);
+			dorm.setResident("unaceptable", roomNum);
+		}
+		else{
+			dorm.setResident("unacceptable", roomNum);
+		}
+		System.out.println("Room " + roomNum + " is unacceptable for habitation.");		
+	}
+	
+	/*public void changeCheckInDeadline(Student student, Time time){ //placeholder to allow student to check ino beyond checkin deadline
+		student.setCheckInDeadline(time);
+	}*/
+
+}
---
+++ Student.java
@@ -0,0 +1,170 @@
+import java.util.Scanner;
+
+/*
+ * 
+ * ●	Students  - Malcolm W. 
+o	During registration
+▪	View available rooms ONLY during registration period
+▪	Choose room ONLY during registration period (updates prospective roster)
+▪	Switch rooms ONLY during registration period
+▪	View names of residents ONLY during registration period
+o	During move in
+▪	Swipe/enter ID number:
+●	Check into dorm within week of classes starting (updates roster)
+●	Displays student validation and residency status
+▪	Approve room selection
+o	During move out
+▪	Swipe to complete checkout
+o	Any time
+▪	Make maintenance request
+▪	Swipe to approve room change
+
+ */
+public class Student {
+	private Dorm assignedDorm;
+//	private Maintenance mRequest;
+	private boolean isRegestr=true;
+	private boolean isMoveIn=false;
+	private boolean isMoveOut=false;
+	private int validation;
+	private String firstN, middleN, lastN;
+	private String newline= System.getProperty("line.separator");
+	Scanner in = new Scanner(System.in);
+	Student()
+	{
+		if(isRegestr)
+		{
+			registraion();
+		}
+		if(isMoveIn)
+		{
+			movedIn();
+		}
+		if(isMoveOut)
+		{
+			
+		}
+	}
+	Student(Dorm dormStay)
+	{
+		assignedDorm=dormStay;
+	}
+	
+	public void registraion()
+	{
+		int optionChoice;
+		System.out.println("1)View Rooms" + newline + "2)Reserve room" +newline+ "3)View residents");
+		optionChoice = in.nextInt();
+		switch (optionChoice) {
+        case 1:  viewRooms();
+                 break;
+        case 2:  reserveRoom();
+                 break;
+        case 3:  viewResidents();
+                 break;
+    }
+	}
+	public void movedIn()
+	{
+		int optionChoice;
+		System.out.println("1)Enter ID" + newline + "2)Check in To Room" +newline+ "3)Switch Rooms "+newline+ "4)Validation/Residency Status" + newline + "5)Approve Room"+ newline +"6)Checkout");
+		optionChoice = in.nextInt();
+		switch (optionChoice) {
+        case 1:  idInput();
+                 break;
+        case 2:  checkInto();
+                 break;
+        case 3:  switchRoom();
+                 break;
+        case 4:  validationRes();
+        		 break;
+        case 5:  approveRoom();
+		 		 break;
+        case 6:  checkOut();
+        	     break;
+		}
+	}
+	public void checkOut()
+	{
+		int optionChoice;
+		System.out.println("1)Checkout");
+		checkOutRoom();
+	}
+	public void idInput()
+	{
+		//assignedDorm.getResident(r);
+	}
+	public void checkInto()
+	{
+		//assignedDorm.checkInto();
+	}
+	
+	public void checkOutRoom()
+	{
+	//	assignedDorm.checkOut();
+	}
+	
+	public void validationRes()
+	{
+	//	assignedDorm.validationRES();
+	}
+	public void approveRoom()
+	{
+	//	assignedDorm.getRoom();
+	}
+
+	public void setRegestr(boolean regestrTime)
+	{
+		isRegestr=regestrTime;
+	}
+	public void viewRooms()
+	{	
+			System.out.println( assignedDorm.getAvailRooms());
+	}
+	public void reserveRoom()
+	{
+		//if(isRegestr==true)
+		//{
+			System.out.println( assignedDorm.getAvailRooms());
+			// assignedDorm.setResident(res, room);
+		//}
+	//	 System.out.println("Viewing availble rooms only available during regisrtation period.");
+	}
+	public void viewResidents()
+	{
+		//if(isRegestr==true)
+		//{
+			System.out.println( assignedDorm.getRoster());
+			// assignedDorm.setResident(res, room);
+		//}
+	//	 System.out.println("Viewing availble rooms only available during regesrtation period.");
+	}
+	
+	public void switchRoom()
+	{
+		//if(isRegestr==true)
+		//{
+			//System.out.println( assignedDorm.switchRoom());
+			// assignedDorm.setResident(res, room);
+		//}
+		// System.out.println("Viewing availble rooms only available during regesrtation period.");
+	}
+	public boolean getRegestr()
+	{
+		return isRegestr;
+	}
+	public boolean getMovedIn()
+	{
+		return isMoveIn;
+	}
+	public boolean getMovedout()
+	{
+		return isMoveOut;
+	}
+	public void maintenanceR(Maintenance mRequest)
+	{
+		//mRequest.request("Please fix my sink");
+	}
+	
+	
+}
